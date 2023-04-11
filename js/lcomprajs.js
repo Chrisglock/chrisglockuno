1137ae74b73bf535b28f55e3989a15eb8ebb87c8
Chrisglock <cristianramos064@gmail.com>
2023-04-10 21:28:29 -0400

    cambios moviles a medio hacer

--- js/lcomprajs.js
+++ js/lcomprajs.js
@@ -25,19 +25,18 @@ document.getElementById('add-product-form').addEventListener('submit', function(
 
 // Add event listener to the product list to handle editing and deleting
 productList.addEventListener('click', function(event) {
-  if (event.target.matches('.edit-btn')) {
-    // Edit button clicked
+
+  if (event.target.matches('.productInput')) {
+
     const index = parseInt(event.target.dataset.index);
     const product = products[index];
-    var checkbox = document.getElementById("checkbox"+String(index));
+    console.log(index)
+    // Toggle the completed status of the product
+    //product.completed = !product.completed;
 
-    if (!checkbox.checked){
-      const newPrice = prompt('Agrega el nuevo precio ' + product.name + ':', product.price);
-      product.price = parseFloat(newPrice);
-    }
-      // Render the updated product list
-      renderProducts();
-    
+    // Update the total price
+    total += product.price*product.cantidad;
+    totalPrice.textContent = "$ "+ total.toFixed(2);
     
   } else if (event.target.matches('.delete-btn')) {
     // Delete button clicked
@@ -46,24 +45,9 @@ productList.addEventListener('click', function(event) {
     total +=products[index].completed ? -products[index].price*product.cantidad : 0;
     totalPrice.textContent ="$ "+  total.toFixed(2);
     products.splice(index, 1);
-    //if(products==[]){total=0;totalPrice.textContent = total.toFixed(2);}
-    // Render the updated product list
-    renderProducts();
-  } else if (event.target.matches('.completed-checkbox')) {
-    // Checkbox clicked
-    const index = parseInt(event.target.dataset.index);
-    const product = products[index];
-
-    // Toggle the completed status of the product
-    product.completed = !product.completed;
-
-    // Update the total price
-    total += product.completed ? product.price*product.cantidad : -product.price*product.cantidad;
-    totalPrice.textContent = "$ "+ total.toFixed(2);
-
-    // Render the updated product list
     renderProducts();
   }
+
 });
 
 // Render the product list
@@ -79,16 +63,34 @@ function renderProducts() {
   
       // Add the product name
       const nameCell = document.createElement('td');
-      nameCell.textContent = product.name;
+      var nameCellinput = document.createElement('input');
+      nameCellinput.classList.add('form-control');
+      nameCellinput.value = product.name;
+      nameCell.appendChild(nameCellinput);
       row.appendChild(nameCell);
+      
       // Add the product quantity
-      const cantCell = document.createElement('td');
-      cantCell.textContent = product.cantidad;
-      row.appendChild(cantCell); 
+      const cantCell = document.createElement('td');//create the column
+      var cantCellinput = document.createElement('input');//create input cell
+      cantCellinput.classList.add('form-control','productInput');//add class
+      cantCell.setAttribute('data-index', index);
+      cantCellinput.value = product.cantidad;//add value to the input
+      cantCell.appendChild(cantCellinput);//add the input elemnt to the columg
+      row.appendChild(cantCell); //add the column to the row
       
       // Add the product price
       const priceCell = document.createElement('td');
-      priceCell.textContent = "$ "+  product.price;
+      const cashSign = document.createElement('label');
+      priceCell.setAttribute('data-index', index);
+      cashSign.textContent = "$";
+      priceCell.setAttribute('id', 'pricetd');
+      priceCell.appendChild(cashSign);
+      
+      //priceCell.textContent = "$";
+      var priceCellinput = document.createElement('input');//create input cell
+      priceCellinput.classList.add('form-control','productInput');//add class
+      priceCellinput.value = product.price;//add value to the input
+      priceCell.appendChild(priceCellinput);//add the input elemnt to the columg
       row.appendChild(priceCell);
   
       // Add the edit button
@@ -97,28 +99,28 @@ function renderProducts() {
       editButton.classList.add('btn', 'btn-secondary', 'edit-btn');
       editButton.textContent = 'Editar';
       editButton.setAttribute('data-index', index);
-      editCell.appendChild(editButton);
-      row.appendChild(editCell);
+      //editCell.appendChild(editButton);
+      //row.appendChild(editCell);
   
       // Add the delete button
-      const deleteCell = document.createElement('td');
+      //const deleteCell = document.createElement('td');
       const deleteButton = document.createElement('button');
       deleteButton.classList.add('btn', 'btn-danger', 'delete-btn');
       deleteButton.textContent = 'Borrar';
       deleteButton.setAttribute('data-index', index);
-      deleteCell.appendChild(deleteButton);
-      row.appendChild(deleteCell);
+      editCell.appendChild(deleteButton);
+      //row.appendChild(editCell);
   
       // Add the completed checkbox
-      const completedCell = document.createElement('td');
+      //const completedCell = document.createElement('td');
       const completedCheckbox = document.createElement('input');
       completedCheckbox.setAttribute('type', 'checkbox');
       completedCheckbox.setAttribute('id', 'checkbox'+String(index));
       completedCheckbox.classList.add('completed-checkbox');
       completedCheckbox.checked = product.completed;
       completedCheckbox.setAttribute('data-index', index);
-      completedCell.appendChild(completedCheckbox);
-      row.appendChild(completedCell);
+      editCell.appendChild(completedCheckbox);
+      row.appendChild(editCell);//agregar el borrar y el checkbox al row
   
       // Cross out the product name and price if completed
       if (product.completed) {
